7e7d04ffb79fd817ed140a056dbad13bdb99efeb
jcdev21 <jcdev21@gmail.com>
2023-10-04 10:57:50 +0700

    mysql to postgressql

--- prisma/migrations/20231003215711_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `verification_tokens` DROP FOREIGN KEY `verification_tokens_identifier_fkey`;
-
--- AddForeignKey
-ALTER TABLE `verification_tokens` ADD CONSTRAINT `verification_tokens_identifier_fkey` FOREIGN KEY (`identifier`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
